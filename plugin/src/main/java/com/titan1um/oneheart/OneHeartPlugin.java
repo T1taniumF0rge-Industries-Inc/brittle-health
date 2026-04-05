521e679e2f434e5707c964c5e4a03e1a961f1a90
Titan1um <gamersoftware.corp@gmail.com>
2026-04-06 00:30:08 +0200

    Integrate bStats metrics tracking
    Added bStats metrics for player gamemodes and online players.

--- plugin/src/main/java/com/titan1um/oneheart/OneHeartPlugin.java
+++ plugin/src/main/java/com/titan1um/oneheart/OneHeartPlugin.java
@@ -2,6 +2,7 @@ package com.titan1um.oneheart;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public final class OneHeartPlugin extends JavaPlugin {
 
@@ -16,7 +17,25 @@ public final class OneHeartPlugin extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new JoinListener(), this);
 
         getLogger().info("[One Heart] Loaded!");
-    }
+
+        // bStats metrics
+        int pluginId = 30593;
+        Metrics metrics = new Metrics(this, pluginId);
+
+        metrics.addCustomChart(new Metrics.AdvancedPie("player_gamemodes", () -> {
+            Map<String, Integer> gamemodeMap = new HashMap<>();
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                String gamemode = player.getGameMode().name();
+                gamemodeMap.put(gamemode, gamemodeMap.getOrDefault(gamemode, 0) + 1);
+            }
+            return gamemodeMap;
+
+        metrics.addCustomChart(new MultiLineChart("players_and_servers", () -> {
+            Map<String, Integer> valueMap = new HashMap<>();
+            valueMap.put("servers", 1);
+            valueMap.put("players", Bukkit.getOnlinePlayers().size());
+            return valueMap;
+        }
 
     public boolean isEnabledState() {
         return enabled;
